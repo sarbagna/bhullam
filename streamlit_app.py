3eca19d8675a400882c8c0e8dec54d83fba61d62
Arijit Sarbagna <sarbagna@gmail.com>
2023-09-27 12:51:26 +0000

    Dataframes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,25 @@
 import streamlit as st
+import pandas as pd
+
 from langchain.llms import OpenAI
 
 st.title('🦜🔗 Quickstart App')
 
 animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
-
 animal = st.text_input('Type an animal')
-
 if st.button('Check availability'):
     have_it = animal.lower() in animal_shelter
     'We have that animal!' if have_it else 'We don\'t have that animal.'
+
+df = pd.DataFrame(
+    [
+        {"command": "st.selectbox", "rating": 4, "is_widget": True},
+        {"command": "st.balloons", "rating": 5, "is_widget": False},
+        {"command": "st.time_input", "rating": 3, "is_widget": True},
+    ]
+)
+
+st.dataframe(df, use_container_width=True)
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
